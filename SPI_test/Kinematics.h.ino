9669d7bca142fa126591561dc5140c30e6c6680d
udoku <40163565+udoku@users.noreply.github.com>
2018-06-13 01:37:30 -0700

    Updates to kinematics files
    Sketched out Kinematics class and wrote arctangent approximation and coordinate system conversion functions

--- SPI_test/Kinematics.h.ino
+++ SPI_test/Kinematics.h.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Arduino.h"
 
-}
+class Kinematics {
+  public:
+    Kinematics(); //Initializes class
+    //set motor positions given location input
+    
+  private:
+    //home positions of motors A, B, C
+    //desired positional of motors A, B, C
+    float theta; //elevation
+    float phi; //azimuth
+    void _coordinate_transform(float, float, float); //convert (x,y,z) location to (r,theta,phi) location
+    //convert (r,theta,phi) location to tendon lengths
+    //convert tendon lengths to calls of set_rotations
+    float _arctangent(float); //approximation of arctangent
+};
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
--- SPI_test/Kinematics.ino
+++ SPI_test/Kinematics.ino
@@ -1,22 +1,28 @@
-#include "TMC5130A.h"
-#include <SoftwareSerial.h>
+//#include "TMC5130A.h"
 
-// statically define a motor object. 
-TMC5130A motorDriverC(10, 32, 9, 49, 48);
+#define MAX_ROTATIONS 20
+#define MIN_ROTATIONS -20
 
-void setup() {
-  // must be called before your program runs. 
-  motorDriverC.setup();
+/*
+ * approximation of arctangent
+ */
+float Kinematics::_arctangent(float arg){
+  float abs_arg = abs(arg);
+  float ans;
+  if (abs_arg<1) ans = (abs_arg - (pow(abs_arg,3)/3) + (pow(abs_arg,5)/5)); // beginning of the Taylor series for artangent(arg) about 0
+  else if (abs_arg<3) ans = (1.107 + (abs_arg-2)/5 - (2/25 * pow((abs_arg-2),2))); // beginning of the Taylor series for artangent(arg) about 2
+  else if (abs_arg<8) ans = 1.5708 - 0.322 * (8 - abs_arg); // linear interpolation between arctangent(3) and arctangent(infinity)
+  else ans = 1.5708; // after about 8, arctangent is pretty close to Pi/2
 
-  //can be called any time before you wish the motor to move. 
-  motorDriverC.enable();
+  return (arg / abs_arg * ans);
 }
-void loop() {
-  motorDriverC.set_rotations(1.0);
-  delay(3500);
-  motorDriverC.set_rotations(0.0);
-  delay(2500);
-  motorDriverC.set_rotations(-1.0);
-  delay(2500);
+
+/*
+ * convert (x,y,z) location to (r,theta,phi) location
+ */
+void Kinematics::_coordinate_transform(float x, float y, float z){
+    theta = _arctangent(pow((x*x+y*y),0.5)/z); // determines the elevation (theta = 0 => straight down)
+    if (x>0) phi = _arctangent(y/x); // determines the azimuth
+    else phi = _arctangent(y/x) + 1.5708; // plus Pi/2
 }
 
