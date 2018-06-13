10b8199274282680e644b576347436fdf588a3ef
udoku <40163565+udoku@users.noreply.github.com>
2018-06-13 00:12:38 -0700

    Created Kinematics files

---
+++ SPI_test/Kinematics.ino
@@ -0,0 +1,22 @@
+#include "TMC5130A.h"
+#include <SoftwareSerial.h>
+
+// statically define a motor object. 
+TMC5130A motorDriverC(10, 32, 9, 49, 48);
+
+void setup() {
+  // must be called before your program runs. 
+  motorDriverC.setup();
+
+  //can be called any time before you wish the motor to move. 
+  motorDriverC.enable();
+}
+void loop() {
+  motorDriverC.set_rotations(1.0);
+  delay(3500);
+  motorDriverC.set_rotations(0.0);
+  delay(2500);
+  motorDriverC.set_rotations(-1.0);
+  delay(2500);
+}
+
